6d478adc19d07ac809c2799e90a7c95d898cf764
GreatDivine <kylestrader94@gmail.com>
2015-09-18 12:17:22 -0400

    Snoop dogg

--- BirthdayCard/app/src/main/java/com/example/kylestrader/birthdaycard/MainActivity.java
+++ BirthdayCard/app/src/main/java/com/example/kylestrader/birthdaycard/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.kylestrader.birthdaycard;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,8 +48,68 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        if (id == R.id.action_send) {
+            //take screenshot
+            Bitmap myBitmap = takeScreenshot();
+            //save screenshot to the device
+            String filePath = saveScreenshot(myBitmap);
+
+            //send email with the screenshot as an attachment
+            sendMail(filePath);
+
+            return true;
+        }
+
+
         return super.onOptionsItemSelected(item);
     }
+    public Bitmap takeScreenshot()
+    {
+        View view = findViewById(android.R.id.content);
+        view.setDrawingCacheEnabled(true);
+
+        Bitmap myBitmap = view.getDrawingCache();
+        return myBitmap;
+    }
+
+    public String saveScreenshot(Bitmap bitmap) {
+        String filePath = Environment.getExternalStorageDirectory() +
+                File.separator + "Pictures/screenshot.png";
+        File imagePath = new File(filePath);
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(imagePath);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.e("MainActivity", e.getMessage(), e);
+        } catch (IOException e) {
+            Log.e("MainActivity", e.getMessage(), e);
+        }
+        return filePath;
+    }
+
+    public void sendMail(String path) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Happy Birthday");
+        Uri attachment = Uri.parse("file://" + path);
+        intent.putExtra(Intent.EXTRA_STREAM, attachment);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else
+        {
+            //give me a toast
+            Context context = getApplicationContext();
+            String text = "No Email app Available";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+
 
     public void create(View view) {
         EditText toField = (EditText) findViewById(R.id.EditTextTo);
@@ -48,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         Editable fromEditable = fromField.getText();
         String sender = fromEditable.toString();
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_card_render);
 
         TextView to_tv = (TextView) findViewById(R.id.to_tv);
         to_tv.setText("Happy Birthday, " + recipient);
