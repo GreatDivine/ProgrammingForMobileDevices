79ac3e1d3836082f2898f7e21c40a6615e9afefa
GreatDivine <kylestrader94@gmail.com>
2015-09-22 12:05:41 -0400

    Added intent filter

---
+++ Intent-Filter/app/src/main/java/com/example/kylestrader/intent_filter/SecondActivity.java
@@ -0,0 +1,57 @@
+package com.example.kylestrader.intent_filter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        String value1 = extras.getString("Value1");
+        String value2 = extras.getString("Value2");
+
+        Context context = getApplicationContext();
+        String message = "First value: " + value1 + "\nSecond value: " + value2;
+
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void goHome(View view)
+    {
+        Context context = getApplicationContext();
+        Intent i = new Intent(context, MainActivity.class);
+        startActivity(i);
+    }
+}
