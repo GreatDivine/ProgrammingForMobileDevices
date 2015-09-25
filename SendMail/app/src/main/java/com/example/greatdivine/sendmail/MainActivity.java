205058af12a173d9ec1d4d62b88e23db70ecf3d0
GreatDivine <kylestrader94@gmail.com>
2015-09-25 12:35:36 -0400

    hj

--- SendMail/app/src/main/java/com/example/greatdivine/sendmail/MainActivity.java
+++ SendMail/app/src/main/java/com/example/greatdivine/sendmail/MainActivity.java
@@ -2,19 +2,45 @@ package com.example.greatdivine.sendmail;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final int PICK_FROM_GALLERY = 101;
+    Uri URI = null;
+    int columnIndex;
+    String attachmentFile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PICK_FROM_GALLERY && resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            attachmentFile = cursor.getString(columnIndex);
+            URI = Uri.parse("android:resource//" + attachmentFile);
+            cursor.close();
+        }
     }
 
     @Override
@@ -41,12 +67,22 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public void sendMail() {
+    public void sendMail(View view) {
+
+        String email = ((EditText) findViewById(R.id.email_address_view)).getText().toString();
+        String emailArr[] = {email};
+
+        String subject = ((EditText) findViewById(R.id.subject_view)).getText().toString();
+        String message = ((EditText) findViewById(R.id.message_view)).getText().toString();
+
         Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:"));
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Happy Birthday");
-        //Uri attachment = Uri.parse("file://" + path);
-        //intent.putExtra(Intent.EXTRA_STREAM, attachment);
+        intent.putExtra(Intent.EXTRA_EMAIL, emailArr);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (URI != null) {
+            intent.putExtra(Intent.EXTRA_STREAM, URI);
+        }
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
@@ -60,4 +96,15 @@ public class MainActivity extends AppCompatActivity {
             toast.show();
         }
     }
+
+    public void openGallery(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra("return-data", true);
+        startActivityForResult(
+                Intent.createChooser(intent, "Complete action using"),
+                PICK_FROM_GALLERY);
+    }
+
 }
