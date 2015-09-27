ef18960553cdceeaccd8097f7a70d90c9366e369
GreatDivine <kylestrader94@gmail.com>
2015-09-27 14:40:15 -0400

    Finished send mail assignment

--- SendMail/app/src/main/java/com/example/greatdivine/sendmail/MainActivity.java
+++ SendMail/app/src/main/java/com/example/greatdivine/sendmail/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.greatdivine.sendmail;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -11,35 +12,42 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.graphics.Matrix;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-
-    private static final int PICK_FROM_GALLERY = 101;
+    EditText editTextEmail, editTextSubject, editTextMessage;
+    Button send, attachment;
+    String email, message, subject, attachmentFile;
     Uri URI = null;
+    private static final int PICK_FROM_GALLERY = 101;
     int columnIndex;
-    String attachmentFile;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextEmail = (EditText) findViewById(R.id.email_address_view);
+        editTextSubject = (EditText) findViewById(R.id.subject_view);
+        editTextMessage = (EditText) findViewById(R.id.message_view);
+
+        send = (Button) findViewById(R.id.send);
+        attachment = (Button) findViewById(R.id.attachment);
+
+        send.setOnClickListener(this);
+        attachment.setOnClickListener(this);
     }
 
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == PICK_FROM_GALLERY && resultCode == RESULT_OK) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
-            cursor.moveToFirst();
-            columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            attachmentFile = cursor.getString(columnIndex);
-            URI = Uri.parse("android:resource//" + attachmentFile);
-            cursor.close();
+            URI = data.getData();
         }
     }
 
@@ -69,35 +77,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMail(View view) {
 
-        String email = ((EditText) findViewById(R.id.email_address_view)).getText().toString();
-        String emailArr[] = {email};
+        String email = editTextEmail.getText().toString();
+        String subject = editTextSubject.getText().toString();
+        String message = editTextMessage.getText().toString();
 
-        String subject = ((EditText) findViewById(R.id.subject_view)).getText().toString();
-        String message = ((EditText) findViewById(R.id.message_view)).getText().toString();
+        final Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("plain/text");
 
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.putExtra(Intent.EXTRA_EMAIL, emailArr);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {email});
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        intent.putExtra(Intent.EXTRA_TEXT, message);
+
         if (URI != null) {
             intent.putExtra(Intent.EXTRA_STREAM, URI);
         }
 
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        this.startActivity(Intent.createChooser(intent, "Sending email..."));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == findViewById(R.id.attachment)) {
+            openGallery();
         }
-        else
-        {
-            //give me a toast
-            Context context = getApplicationContext();
-            String text = "No Email app Available";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
+        if (v == findViewById(R.id.send)) {
+            sendMail(v);
         }
     }
 
-    public void openGallery(View view) {
+
+    public void openGallery() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
