8f89bd68de76f162fe5ffd42b4199f68561a15c2
GreatDivine <kylestrader94@gmail.com>
2015-11-06 12:13:06 -0500

    Assignment 7 url connection fix

--- Assignment7/app/src/main/java/com/example/kylestrader/assignment7/MainActivity.java
+++ Assignment7/app/src/main/java/com/example/kylestrader/assignment7/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.kylestrader.assignment7;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +12,7 @@ import android.view.MenuItem;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -21,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        RetrieveMovieList();
     }
 
     @Override
@@ -41,19 +49,19 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    public void RetrieveMovieList()
+    public String RetrieveMovieList()
     {
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
-        String forecastJsonStr = null;
+        String MovieJsonStr = "";
 
         try
         {
             String query = "https://api.themoviedb.org/3/discover/movie?primary_release_year=" + m_BaseYear + "&sort_by=vote_average.desc&api_key=" + m_ApiKey;
+            Log.e("URL", query);
             URL url = new URL(query);
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
@@ -61,13 +69,53 @@ public class MainActivity extends AppCompatActivity {
             InputStream inputStream = urlConnection.getInputStream();
             if(inputStream == null)
             {
-
+                return null;
             }
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuffer buffer = new StringBuffer();
+            String line;
+
+            while((line = reader.readLine()) != null)
+            {
+                buffer.append(line + "/n");
+            }
+
+            if (buffer.length() == 0)
+            {
+                return null;
+            }
+
+            MovieJsonStr = buffer.toString();
         }
 
         catch (IOException e)
         {
             Log.e("Main Activity", "Error", e);
         }
+
+        finally
+        {
+            if(urlConnection != null)
+            {
+                urlConnection.disconnect();
+            }
+
+            if(reader != null)
+            {
+              try
+              {
+                  reader.close();
+              }
+
+              catch(final IOException e)
+              {
+                  Log.e("MainActivity", "Error closing stream", e);
+              }
+            }
+        }
+
+        return null;
     }
 }
